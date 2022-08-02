7ab991cd0b6af1e3d55cda7c4bb1002a7a24ccf9
Rugved <Rugved@DESKTOP-3ETN7D0>
2022-08-02 13:03:02 +0530

    first commit

---
+++ Day-1_Git and GitHub/src/com/capgemini/github/EclipseWithGithubConnection.java
@@ -0,0 +1,11 @@
+package com.capgemini.github;
+//demo for to connect eclipse with github
+public class EclipseWithGithubConnection {
+
+	public static void main(String[] args) {
+		//print the line
+		System.out.println("welcome to capgemini C2TC program");
+
+	}
+
+}
